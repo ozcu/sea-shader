265fee1fdc3e6801e6e4dd6888d01c20a9f27d01
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-11-14 20:54:41 +0300

    Add sea shader

--- src/script.js
+++ src/script.js
@@ -2,27 +2,71 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import waterVertexShader from './shaders/water/vertex.glsl'
+import waterFragmentShader from './shaders/water/fragment.glsl'
+
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI({ width: 340 })
+const debugObject={}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color( 0xffffff )
 
 /**
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(15, 5,512, 512)
+
+
+//Colors
+debugObject.depthColor = '#186691'
+debugObject.surfaceColor = '#9bd8ff'
 
 // Material
-const waterMaterial = new THREE.ShaderMaterial()
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader:waterVertexShader,
+    fragmentShader:waterFragmentShader,
+    uniforms:{
+        uBigWavesElevation : {value:0.136},
+        uBigWavesFrequency : {value:new THREE.Vector2(1.5,1.5)},
+        uTime: {value:0},
+        uBigWavesSpeed:{value:0.75},
+        uDepthColor:{value:new THREE.Color(debugObject.depthColor)},
+        uSurfaceColor:{value:new THREE.Color(debugObject.surfaceColor)},
+        uColorOffset:{value:0.08},
+        uColorMultiplier:{value:4},
+        uSmallWavesElevation: { value: 0.08 },
+        uSmallWavesFrequency: { value: 3 },
+        uSmallWavesSpeed: { value: 0.2 },
+        uSmallIterations: { value: 2 },
+    }
+})
+
+//debug GUI
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+
+gui.addColor(debugObject, 'depthColor').onChange(() => { waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor) })
+gui.addColor(debugObject, 'surfaceColor').onChange(() => { waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
+
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallIterations, 'value').min(0).max(5).step(1).name('uSmallIterations')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -56,8 +100,8 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(1, 1, 1)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 50)
+camera.position.set(0, 1, 2)
 scene.add(camera)
 
 // Controls
@@ -78,9 +122,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
+const animateScene = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //animate Water
+    water.material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
@@ -88,8 +135,8 @@ const tick = () =>
     // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    // Call animateScene again on the next frame
+    window.requestAnimationFrame(animateScene)
 }
 
-tick()
+animateScene()
